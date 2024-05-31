db56817d4515a79a244597e6cca243835bdb293b
yo1956 <hm.hr.yossy@gmail.com>
2024-06-01 00:36:38 +0900

    Implementing store_service.go

---
+++ store/store_service.go
@@ -0,0 +1,44 @@
+package store
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/go-redis/redis/v8"
+)
+
+// Define the struct wrapper around raw Redis client
+type StorageService struct {
+	redisClient *redis.Client
+}
+
+// Top level declarations for the storeService and Redis context
+var (
+	storeService = &StorageService{}
+	ctx          = context.Background()
+)
+
+// Note that in a real world usage, the cache duration shouldn't have
+// an expiration time, an LRU policy config should be set where the
+// values that are retrieved less often are purged automatically from
+// the cache and stored back in RDBMS whenever the cache is full
+const CacheDuration = 6 * time.Hour
+
+// Initializing the store service and return a store pointer
+func InitializeStore() *StorageService {
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+
+	pong, err := redisClient.Ping(ctx).Result()
+	if err != nil {
+		panic(fmt.Sprintf("Error init Redis: %v", err))
+	}
+
+	fmt.Printf("\nRedis started successfully: pong message = {%s}", pong)
+	storeService.redisClient = redisClient
+	return storeService
+}
