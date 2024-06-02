6a23e704581cba210898d7cc38a317ad3c40062e
yo1956 <hm.hr.yossy@gmail.com>
2024-06-02 23:03:09 +0900

    Implemented func: base58Encoded

--- shortener/shorturl_generator.go
+++ shortener/shorturl_generator.go
@@ -2,10 +2,24 @@ package shortener
 
 import (
 	"crypto/sha256"
+	"fmt"
+	"os"
+
+	"github.com/itchyny/base58-go"
 )
 
 func sha2560f(input string) []byte {
 	algorighm := sha256.New()
 	algorighm.Write([]byte(input))
 	return algorighm.Sum(nil)
+}
+
+func base58Encoded(bytes []byte) string {
+	encoding := base58.BitcoinEncoding
+	encoded, err := encoding.Encode(bytes)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	return string(encoded)
 }
